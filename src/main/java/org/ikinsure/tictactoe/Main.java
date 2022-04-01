92ebc885b20cf25052ff78efd9800a5c273fdf52
iKinsure <59565620+iKinsure@users.noreply.github.com>
2022-04-01 13:08:02 +0200

    pom.xml update

--- src/main/java/org/ikinsure/tictactoe/Main.java
+++ src/main/java/org/ikinsure/tictactoe/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static final String SEPARATOR = "|";
     public static final String DELIMITER = " ";
     public static final String BORDER = "-";
-    public static final int BOARD_SIZE = 2;
+    public static final int BOARD_SIZE = 3;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
